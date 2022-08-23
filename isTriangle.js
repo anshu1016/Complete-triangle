96b3572b2940c96d6fba0a506d5c943a42f85edd
anshu1016 <69161719+anshu1016@users.noreply.github.com>
2022-08-23 13:28:31 +0530

    Add files via upload

---
+++ isTriangle.js
@@ -0,0 +1,30 @@
+var angles = document.querySelectorAll(".input-angle");
+const checkBtn = document.querySelector("#check-btn")
+const showResult = document.querySelector("#result")
+
+checkBtn.addEventListener("click",validateNum)
+
+function validateNum(){
+    var firstAngle = Number(angles[0].value);
+    var secondAngle = Number(angles[1].value);
+    var thirdAngle = Number(angles[2].value);
+    if( firstAngle >0 && secondAngle>0 && thirdAngle>0 )
+    {
+        const sum = sumofAngles(firstAngle, secondAngle, thirdAngle);
+        if(sum == 180)
+        {
+            showResult.innerText = "Yes your angles makes a complete Triangle which is 180 degree..";
+        }
+        else{
+            showResult.innerText = "Your Complete triangle is incomplete by " +(180-(firstAngle+secondAngle+thirdAngle)) + " degree..";
+
+        }
+    }else{
+        showResult.innerText = "Fill both inputs and make sure that inputs are non negative and greater than zero..";
+    }
+}
+
+function sumofAngles(a,b,c){
+    const total = a+b+c;
+    return total;
+}
